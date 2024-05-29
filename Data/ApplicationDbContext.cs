cc6f9fcb244ff041b82400bed612119061d9a3d0
hale <58865623+ivanovdimitur00@users.noreply.github.com>
2024-05-29 17:47:43 +0300

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using Cybrary.Models;
+
+namespace Cybrary.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Book { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Book>().ToTable("Book");
+
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.HasKey(l => new { l.LoginProvider, l.ProviderKey });
+            });
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cybrary.Models
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Author is required.")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+
+        public string Publisher { get; set; }
+
+        public DateTime Year { get; set; }
+
+        [Required(ErrorMessage = "ISBN is required.")]
+        public string ISBN { get; set; }
+
+        public string Resume { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive value.")]
+        public float Price { get; set; }
+    }
+}
